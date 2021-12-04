55d609a396653d2483bae5ac166b493db06a2986
Felipe Kenzo <felipekenzohamada@gmail.com>
2021-12-04 11:34:11 -0300

    Add todo.service class

---
+++ app/todo.service.php
@@ -0,0 +1,30 @@
+<?php
+
+class TodoService {
+	private $conn;
+	private $todo;
+
+	public function __construct(\PDO $conn) {
+		$this->conn = $conn;
+	}
+
+	public function setTodo($todo) {
+		$this->todo = $todo;
+	}
+
+	public function retriveAll() {
+		$query = '
+			SELECT
+				todos.id, todos.todo, state.description as state
+			FROM 
+				todos LEFT JOIN state ON todos.state = state.state
+		';
+
+		$stmt = $this->conn->prepare($query);
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_OBJ);
+	}
+}
+
+?>
