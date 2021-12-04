d048edfeae8036e1b47dfb6197439c6eafca79c9
Felipe Kenzo <felipekenzohamada@gmail.com>
2021-12-04 11:44:23 -0300

    Make todas_tarefas to fetch Todos from db

--- public/todas_tarefas.php
+++ public/todas_tarefas.php
@@ -1,3 +1,11 @@
+<?php
+	require_once '../app/connection.php';
+	require_once '../app/todo.service.php';
+
+	$service = new TodoService(Connection::connect());
+	$todos = $service->retriveAll();
+?>
+
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -36,23 +44,16 @@
 								<h4>Todas tarefas</h4>
 								<hr />
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+								<?php foreach($todos as $todo) { ?>
+									<div class="row mb-3 d-flex align-items-center tarefa">
+										<div class="col-sm-9"><?= $todo->todo ?> (<?= $todo->state ?>)</div>
+										<div class="col-sm-3 mt-2 d-flex justify-content-between">
+											<i class="fas fa-trash-alt fa-lg text-danger"></i>
+											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-check-square fa-lg text-success"></i>
+										</div>
 									</div>
-								</div>
-
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<?php } ?>
 								
 							</div>
 						</div>
